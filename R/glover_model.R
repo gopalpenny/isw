221a5f86745c87885da0789295470da39d942532
Gopal Penny <gopalpenny@gmail.com>
2024-11-05 05:42:36 -0800

    added dimensionality checks to get_aquifer_drawdown_ratio

--- R/glover_model.R
+++ R/glover_model.R
@@ -118,9 +118,16 @@ get_aquifer_drawdown_ratio <- function(df, r = NULL, K = NULL, D = NULL, V = NUL
       }
     }
   }
+
+  check_dimensionality(K, "ft/s","K")
+  check_dimensionality(D, "ft","D")
+  check_dimensionality(r, "ft","r")
+  check_dimensionality(t, "s","t")
+
   alpha <- K * D / V
-  ### ADD CHECK THAT ALPHA IS A UNITS OBJECT
-  warning('ADD CHECK THAT ALPHA IS A UNITS OBJECT')
+
+  check_dimensionality(alpha, "ft^2/s","alpha")
+
   radius_squared_over_4_alpha_t <- r^2/(4 * alpha * t)
   if (length(units(radius_squared_over_4_alpha_t)$numerator) != 0 | length(units(radius_squared_over_4_alpha_t)$denominator) != 0) {
     stop("Units error resulting in dimensional value input to expint. ",
@@ -131,6 +138,53 @@ get_aquifer_drawdown_ratio <- function(df, r = NULL, K = NULL, D = NULL, V = NUL
 
   s_over_Q <- 1 / (2 * pi * K * D) * exp_integral
   return(s_over_Q)
+}
+
+#' Check dimensionality
+#'
+#' Check dimensionality of object
+#' @param object Object of which to check dimensionality
+#' @param desired_units Required dimensionality, given as units (e.g., "ft/s")
+#' @param variable_name Optional name of the object, for error reporting
+#' @description
+#' Internal function to check the dimensionality (i.e., units) of an object.
+#' First, check that the object is a units object. Second, try to convert the
+#' object to `desired_units` to ensure dimensionality is correct.
+#' @returns
+#' As of right now, nothing is returned
+#' @examples
+#' \dontrun{
+#' object <- set_units(1, "ft")
+#' desired_units <- "m"
+#' check_dimensionality(object, "m") # passes
+#'
+#' # error because object has wrong dimensionality
+#' check_dimensionality(object, "m^2", "object")
+#' check_dimensionality(object, "m^2") # error, note difference in error reporting
+#'
+#' check_dimensionality(1, "m", "object") # error because object not a units object
+#' }
+check_dimensionality <- function(object, desired_units, variable_name = NULL) {
+
+  if (is.null(variable_name)) {
+    variable_name <- "The variable passed to check_dimensionality()"
+  }
+  units::units_options(set_units_mode = "standard")
+  if(!any(class(object)=="units")) {
+    stop(paste(variable_name, 'is not a units object. Make sure to provide appropriate units'))
+  }
+  tryCatch(
+    expr = {
+      test <- units::set_units(object, desired_units)
+    },
+    error = function(cond) {
+      message1 <- paste(variable_name,"is",object,".",
+                    "It should have units that can be converted to",desired_units,
+                    "but the conversion failed\n")
+      message2 <- paste("Here's the original error message:", conditionMessage(cond))
+      stop(paste(message1, message2))
+      NA
+    })
 }
 
 
---
+++ tests/testthat/test-glover-error-checks.R
@@ -0,0 +1,32 @@
+
+object <- units::set_units(1, "ft")
+desired_units <- "m"
+
+test_that("check_dimensionality receives object with wrong dimensionality", {
+  expect_error(check_dimensionality(object, "m^2")) # error because object has wrong dimensionality
+  expect_error(check_dimensionality(object, "hour","object"))
+})
+test_that("check_dimensionality receives a non-units object", {
+  expect_error(check_dimensionality(1, "m")) # error because object not a units object
+  expect_error(check_dimensionality(1, "m", "object"))
+})
+
+
+# Check that units errors work properly for get_aquifer_drawdown_ratio
+D <- units::set_units(100, "ft")
+K <- units::set_units(0.001, "ft/sec")
+t <- units::set_units(5, "year")
+V <- 0.2 # unitless
+r <- units::set_units(1, "mi")
+t <- units::set_units(5, "year")
+
+val <- round(get_aquifer_drawdown_ratio(r = r, K = K, D = D, V = V, t = t),6)
+
+test_that("get_aquifer_drawdown_ratio gives error with wrong input units", {
+  expect_equal(val, units::set_units(-1.540413, "s/ft^2"))
+  expect_error(get_aquifer_drawdown_ratio(r = 1, K = K, D = D, V = V, t = t))
+  expect_error(get_aquifer_drawdown_ratio(r = r, K = 1, D = D, V = V, t = t))
+  expect_error(get_aquifer_drawdown_ratio(r = r, K = K, D = 1, V = set_units(1,"ft"), t = t))
+  expect_error(get_aquifer_drawdown_ratio(r = r, K = K, D = D, V = V, t = 1))
+})
+
